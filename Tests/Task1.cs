24c7c8d5fa9f953fcafd4f09412635a21daf7089
Nikita Grishaev <grishaev@rightway.games>
2020-10-10 20:25:14 +0300

    Тесты для вставки после заданного узла

--- Tests/Task1.cs
+++ Tests/Task1.cs
@@ -1,4 +1,5 @@
-﻿using AlgorithmsDataStructures;
+﻿using System.CodeDom;
+using AlgorithmsDataStructures;
 using NUnit.Framework;
 
 namespace Tests
@@ -86,7 +87,7 @@ namespace Tests
         [TestCase(new []{1, 1, 2, 1, 3, 4, 1}, 1, "2 3 4", 2, 4)]
         [TestCase(new []{2, 3, 4}, 1, "2 3 4", 2, 4)]
         [TestCase(new []{2, 3, 4}, 3, "2 4", 2, 4)]
-        public void RemoveAllest(int[] nodeValues, int valToRemove, string listAfterOperation, int? head, int? tail)
+        public void RemoveAllTest(int[] nodeValues, int valToRemove, string listAfterOperation, int? head, int? tail)
         {
             var testList = CreateLinkedList(nodeValues);
 
@@ -95,6 +96,46 @@ namespace Tests
             Assert.True(testList.ToString().Equals(listAfterOperation));
             Assert.True(CheckHeadAndTail(testList.head, head));
             Assert.True(CheckHeadAndTail(testList.tail, tail));
+        }
+        
+        [TestCase(new []{2, 3, 4}, null, 1, "1 2 3 4", 1, 4)]
+        [TestCase(new []{2, 3, 4}, 0, 1, "2 1 3 4", 2, 4)]
+        [TestCase(new []{2, 3, 4}, 1, 1, "2 3 1 4", 2, 4)]
+        [TestCase(new []{2, 3, 4}, 2, 1, "2 3 4 1", 2, 1)]
+        [TestCase(new []{2, 4}, null, 1, "1 2 4", 1, 4)]
+        [TestCase(new []{2, 4}, 1, 1, "2 4 1", 2, 1)]
+        [TestCase(new []{2}, 0, 1, "2 1", 2, 1)]
+        [TestCase(new []{2}, null, 1, "1 2", 1, 2)]
+        public void InsertAfterTest(int[] nodeValues, int? insertAfterPosition, int insertNodeValue, string resultList, int? head, int? tail)
+        {
+            var testList = CreateLinkedList(nodeValues);
+            Node positionNode = GetNodeAtPosition(testList, insertAfterPosition);
+            
+            testList.InsertAfter(positionNode, new Node(insertNodeValue));
+
+            Assert.True(testList.ToString().Equals(resultList));
+            Assert.True(CheckHeadAndTail(testList.head, head));
+            Assert.True(CheckHeadAndTail(testList.tail, tail));
+        }
+
+        private Node GetNodeAtPosition(LinkedList testList, int? insertAfterPosition)
+        {
+            var curNode = testList.head;
+
+            if (insertAfterPosition == null)
+                return null;
+
+            if (insertAfterPosition.Value > testList.Count())
+                return testList.tail;
+            
+            var counter = 0;
+            while (curNode != null && counter < insertAfterPosition)
+            {
+                curNode = curNode.next;
+                counter++;
+            }
+
+            return curNode;
         }
 
         private bool CheckHeadAndTail(Node node, int? expectedVal)
