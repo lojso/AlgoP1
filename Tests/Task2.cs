b87a8a32cef49e3f9705614f66b54eb4357e1193
Unknown <lojso.gm@gmail.com>
2020-10-12 16:02:41 +0300

    Дополнительные тесты второй задачи

--- Tests/Task2.cs
+++ Tests/Task2.cs
@@ -12,16 +12,18 @@ namespace Tests
         [TestCase(new []{1}, 1, "", true, null, null)]
         [TestCase(new []{1}, 2, "1", false, 1, 1)]
         [TestCase(new []{1, 2}, 3, "1 2", false, 1, 2)]
+        [TestCase(new []{1, 2}, 1, "2", true, 2, 2)]
+        [TestCase(new []{1, 2}, 2, "1", true, 1, 1)]
         [TestCase(new []{1, 2, 3, 4}, 1, "2 3 4", true, 2, 4)]
         [TestCase(new []{1, 2, 3, 4}, 2, "1 3 4", true, 1, 4)]
         [TestCase(new []{1, 2, 3, 4}, 4, "1 2 3", true, 1, 3)]
-        public void RemoveTest(int[] nodeValues, int valToRemove, string listAfterOperation, bool operationValue, int? head, int? tail)
+        public void RemoveTest(int[] nodeValues, int valToRemove, string listAfterOperation, bool result, int? head, int? tail)
         {
             var testList = CreateLinkedList(nodeValues);
 
             var removeOperationResult = testList.Remove(valToRemove);
             
-            Assert.True(removeOperationResult == operationValue);
+            Assert.True(removeOperationResult == result);
             Assert.True(testList.ToString().Equals(listAfterOperation));
             Assert.True(CheckHeadAndTail(testList.head, head));
             Assert.True(CheckHeadAndTail(testList.tail, tail));
