776d3c663706630efc00cb1da5aec72afc34270d
Unknown <lojso.gm@gmail.com>
2020-10-16 00:52:56 +0300

    Задача 3
    Динамический массив

--- AlgoP1/Task3.cs
+++ AlgoP1/Task3.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 
 namespace AlgorithmsDataStructures
 {
-
     public class DynArray<T>
     {
         public T [] array;
         public int count;
         public int capacity;
+        
+        private const int MIN_CAPACITY = 16;
+        private float _decreaceSizeFillPercentage = 0.5f;
 
         public DynArray()
         {
@@ -18,29 +20,114 @@ namespace AlgorithmsDataStructures
 
         public void MakeArray(int new_capacity)
         {
-            // ваш код
+            new_capacity = Math.Max(new_capacity, MIN_CAPACITY);
+            if (array == null)
+            {
+                CreateArray(new_capacity);
+            }
+            else
+            {
+                ResizeArray(new_capacity);
+            }
+            
         }
 
         public T GetItem(int index)
         {
-            // ваш код
-            return default(T);
+            if (!IsCorectIndex(index, false))
+                throw new IndexOutOfRangeException();
+            
+            return array[index];
         }
 
         public void Append(T itm)
         {
-            // ваш код
+            TryIncreaseSize();
+            
+            array[count] = itm;
+            count++;
         }
 
         public void Insert(T itm, int index)
         {
-            // ваш код
+            if (!IsCorectIndex(index, true))
+                throw new IndexOutOfRangeException();
+            
+            if (index == count)
+            {
+                Append(itm);
+                return;
+            }
+            
+            TryIncreaseSize();
+
+            // TODO: чекнуть этот момент, что делать если вставляем на 0 позицию.
+            T[] newArray = new T[capacity];
+            Array.Copy(array, newArray, index);
+            newArray[index] = itm;
+            Array.Copy(array, index, newArray, index + 1, count - index);
+            count++;
+
+            array = newArray;
         }
 
         public void Remove(int index)
         {
-            // ваш код
+            if (!IsCorectIndex(index, false))
+                throw new IndexOutOfRangeException();
+            
+            T[] newArray = new T[capacity];
+            Array.Copy(array, newArray, index);
+            Array.Copy(array, index + 1, newArray, index, count - index - 1);
+            array = newArray;
+            count--;
+            
+            TryReduceSize();
         }
 
+        
+        private bool IsCorectIndex(int index, bool isInclusive)
+        {
+            if(isInclusive)
+                return index >= 0 && index <= count;
+            
+            return index >= 0 && index < count;
+        }
+
+        
+        private void ResizeArray(int newCapacity)
+        {
+            newCapacity = Math.Max(newCapacity, MIN_CAPACITY);
+            T[] newArray = new T[newCapacity];
+            capacity = newCapacity;
+            
+            Array.Copy(array, newArray, Math.Min(newArray.Length, array.Length));
+            array = newArray;
+
+        }
+
+        private void CreateArray(int size)
+        {
+            size = Math.Max(MIN_CAPACITY, size);
+            array = new T[size];
+            capacity = size;
+        }
+        
+        private void TryIncreaseSize()
+        {
+            if(count != capacity)
+                return;
+            
+            ResizeArray(capacity * 2);
+        }
+        
+        private void TryReduceSize()
+        {
+            if((float) count / capacity >= _decreaceSizeFillPercentage)
+                return;
+            
+            ResizeArray((int)(capacity / 1.5f));
+        }
+        
     }
 }
---
+++ Tests/Task3.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using AlgorithmsDataStructures;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Task3
+    {
+        [TestCase(1, 1, 1, 16)]
+        [TestCase(1, 2, 2, 16)]
+        [TestCase(1, 16, 16, 16)]
+        [TestCase(1, 17, 17, 32)]
+        public void CreateArrayTest(int start, int end, int expectedCount, int expectedCapacity)
+        {
+            DynArray<int> array = CreateDynArrayWithRange(start, end);
+            
+            Assert.True(array.count == expectedCount);
+            Assert.True(array.capacity == expectedCapacity);
+        }
+        
+        [TestCase(1, 1, 0, 1, 1, 16)]
+        [TestCase(1, 2, 0, 1, 2, 16)]
+        [TestCase(1, 2, 1, 2, 2, 16)]
+        [TestCase(1, 16, 15, 16, 16, 16)]
+        [TestCase(1, 17, 15, 16, 17, 32)]
+        [TestCase(1, 17, 16, 17, 17, 32)]
+        public void CorrectGetElementTest(
+            int start, int end,
+            int getIndex, int expectedValue,
+            int expectedCount, int expectedCapacity)
+        {
+            DynArray<int> array = CreateDynArrayWithRange(start, end);
+            
+            Assert.True(array.count == expectedCount);
+            Assert.True(array.capacity == expectedCapacity);
+            Assert.True(array.GetItem(getIndex) == expectedValue);
+        }
+        
+        [TestCase(0, -1, 0)]
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 1, -1)]
+        [TestCase(1, 1, 16)]
+        [TestCase(1, 16, 16)]
+        [TestCase(1, 16, -1)]
+        [TestCase(1, 17, 17)]
+        public void IncorrectGetElementTest(
+            int start, int end,
+            int getIndex)
+        {
+            DynArray<int> array = CreateDynArrayWithRange(start, end);
+
+            Assert.That(() => array.GetItem(getIndex), Throws.TypeOf<IndexOutOfRangeException>());
+        }
+        
+        [TestCase(0, -1, 0, 1, 1, 1, 16)]
+        [TestCase(1, 1, 0, 0, 0, 2, 16)]
+        [TestCase(1, 3, 1, 0, 0, 4, 16)]
+        [TestCase(1, 15, 15, 16, 16, 16, 16)]
+        [TestCase(1, 16, 16, 17, 17, 17, 32)]
+        public void CorrectInsertTest(
+            int start, int end,
+            int insertIndex, int insertValue, int expectedValue,
+            int expectedCount, int expectedCapacity)
+        {
+            DynArray<int> array = CreateDynArrayWithRange(start, end);
+            
+            array.Insert(insertValue, insertIndex);
+            
+            Assert.True(array.count == expectedCount);
+            Assert.True(array.capacity == expectedCapacity);
+            Assert.True(array.GetItem(insertIndex) == expectedValue);
+        }
+        
+        [TestCase(1, 3, 0, 2, 16)]
+        [TestCase(1, 3, 1, 2, 16)]
+        [TestCase(1, 4, 1, 3, 16)]
+        [TestCase(1, 3, 2, 2, 16)]
+        public void CorrectRemoveTest(
+            int start, int end,
+            int removeIndex,
+            int expectedCount, int expectedCapacity)
+        {
+            DynArray<int> array = CreateDynArrayWithRange(start, end);
+            
+            array.Remove(removeIndex);
+            
+            Assert.True(array.count == expectedCount);
+            Assert.True(array.capacity == expectedCapacity);
+        }
+        
+        [TestCase(1, 32, 30, 2, 16)]
+        [TestCase(1, 32, 16, 16, 32)]
+        [TestCase(1, 32, 17, 15, 21)]
+        public void RemoveDecreaseBufferTest(
+            int start, int end,
+            int removeNum,
+            int expectedCount, int expectedCapacity)
+        {
+            DynArray<int> array = CreateDynArrayWithRange(start, end);
+
+            for (int i = 0; i < removeNum; i++)
+            {
+                array.Remove(0);
+            }
+            
+            Assert.True(array.count == expectedCount);
+            Assert.True(array.capacity == expectedCapacity);
+        }
+        
+        [TestCase(0, -1, 0)]
+        [TestCase(1, 1, 1)]
+        [TestCase(1, 1, -1)]
+        [TestCase(1, 1, 16)]
+        [TestCase(1, 16, 16)]
+        [TestCase(1, 16, -1)]
+        [TestCase(1, 17, 17)]
+        public void IncorrectRemoveTest(
+            int start, int end,
+            int removeIndex)
+        {
+            DynArray<int> array = CreateDynArrayWithRange(start, end);
+
+            Assert.That(() => array.Remove(removeIndex), Throws.TypeOf<IndexOutOfRangeException>());
+        }
+        
+
+        [TestCase(0, -1, 1, 2)]
+        [TestCase(1, 1, 2, 2)]
+        [TestCase(1, 1, -1, 2)]
+        [TestCase(1, 1, 40, 40)]
+        [TestCase(1, 1, 16, 2)]
+        [TestCase(1, 16, 17, 2)]
+        [TestCase(1, 18, 20, 2)]
+        [TestCase(1, 16, -1, 2)]
+        public void IncorrectInsertTest(
+            int start, int end,
+            int insertIndex, int insertValue)
+        {
+            DynArray<int> array = CreateDynArrayWithRange(start, end);
+
+            Assert.That(
+                () => array.Insert(insertValue, insertIndex),
+                Throws.TypeOf<IndexOutOfRangeException>()
+                );
+        }
+
+        private DynArray<int> CreateDynArrayWithRange(int start, int end)
+        {
+            DynArray<int> result = new DynArray<int>();
+
+            for (int i = start; i <= end; i++)
+            {
+                result.Append(i);
+            }
+
+            return result;
+        }
+    }
+}
