8903839115d3505a27b87befc64da695fae1d914
Unknown <lojso.gm@gmail.com>
2020-10-16 20:02:47 +0300

    Шаблон 4 задачи

---
+++ AlgoP1/Task4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Stack<T>
+    {
+        public Stack()
+        {
+            // инициализация внутреннего хранилища стека
+        } 
+
+        public int Size() 
+        {
+            // размер текущего стека		  
+            return 0;
+        }
+
+        public T Pop()
+        {
+            // ваш код
+            return default(T); // null, если стек пустой
+        }
+	  
+        public void Push(T val)
+        {
+            // ваш код
+        }
+
+        public T Peek()
+        {
+            // ваш код
+            return default(T); // null, если стек пустой
+        }
+    }
+
+}
