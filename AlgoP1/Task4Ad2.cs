c3e01dfe3677dd48fd4d46cef5d782b992a8c524
Unknown <lojso.gm@gmail.com>
2020-10-21 13:57:26 +0300

    Задача 4. Изменение доп задачи 2

--- AlgoP1/Task4Ad2.cs
+++ AlgoP1/Task4Ad2.cs
@@ -7,24 +7,63 @@ namespace Task4Ad2
     {
         public static bool Parse(string brackets)
         {
+            AlgorithmsDataStructures.Stack<char> bracketsStack = new AlgorithmsDataStructures.Stack<char>();
             AlgorithmsDataStructures.Stack<bool> stack = new AlgorithmsDataStructures.Stack<bool>();
 
+            // Проверяем что правых скобочек сбалансированное количество
             foreach (var bracket in brackets)
             {
-                if (bracket == '(')
+                bracketsStack.Push(bracket);
+            }
+            
+            while (bracketsStack.Size() != 0)
+            {
+                while (bracketsStack.Peek() == ')')
                 {
+                    bracketsStack.Pop();
                     stack.Push(true);
                 }
-                else
-                {
-                    if (stack.Size() == 0)
-                         return false;
 
+                while (bracketsStack.Peek() == '(')
+                {
+                    bracketsStack.Pop();
                     stack.Pop();
                 }
             }
 
-            return stack.Size() == 0;
+            for (int i = 0; i < stack.Size(); i++)
+                return false;
+            
+            // Проверяем что левых скобочек сбалансированное количество
+            bracketsStack = new AlgorithmsDataStructures.Stack<char>();
+            stack = new AlgorithmsDataStructures.Stack<bool>();
+
+            for (var index = brackets.Length - 1; index >= 0; index--)
+            {
+                var bracket = brackets[index];
+                bracketsStack.Push(bracket);
+            }
+
+            while (bracketsStack.Size() != 0)
+            {
+                while (bracketsStack.Peek() == '(')
+                {
+                    bracketsStack.Pop();
+                    stack.Push(true);
+                }
+
+                while (bracketsStack.Peek() == ')')
+                {
+                    bracketsStack.Pop();
+                    stack.Pop();
+                }
+            }
+
+            for (int i = 0; i < stack.Size(); i++)
+                return false;
+            
+            // Если всех скобочек сбалансированно то true
+            return true;
         }
     }
 }
