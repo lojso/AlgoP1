fa8595ccf56178458917bb08cbe8530a630d439e
Unknown <lojso.gm@gmail.com>
2020-10-23 21:55:50 +0300

    Задача 6. Дополнительная задача

---
+++ AlgoP1/Task6Ad1.cs
@@ -0,0 +1,33 @@
+﻿using AlgorithmsDataStructures;
+
+namespace AlgoP1
+{
+    public static class Task6Ad1
+    {
+        private static Deque<char> d;
+
+        public static bool IsPalindrome(string word)
+        {
+            d = StringToDeque(word);
+            int i = 0;
+            while (d.Size() > 0)
+            {
+                if (word[i] != d.RemoveTail())
+                    return false;
+                i++;
+            }
+
+            return true;
+        }
+
+        private static Deque<char> StringToDeque(string word)
+        {
+            var result = new Deque<char>();
+            for (int i = 0; i < word.Length; i++)
+            {
+                result.AddTail(word[i]);
+            }
+            return result;
+        }
+    }
+}
