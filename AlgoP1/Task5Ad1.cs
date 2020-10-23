6c84f4d8d14ac1958aba57735ea72fc32e863fda
Unknown <lojso.gm@gmail.com>
2020-10-23 21:14:28 +0300

    Задача 5. Очередь на двух стеках.

---
+++ AlgoP1/Task5Ad1.cs
@@ -0,0 +1,67 @@
+﻿using AlgorithmsDataStructures;
+
+namespace AlgoP1
+{
+    public class Task5Ad1
+    {
+        public class Queue<T>
+        {
+            private Stack<T> stack
+            {
+                get { return _isNowPush ? _pushStack : _popStack; }
+            }
+
+            private Stack<T> _pushStack;
+            private Stack<T> _popStack;
+
+            private bool _isNowPush = true;
+
+            public Queue()
+            {
+                _pushStack = new Stack<T>();
+                _popStack = new Stack<T>();
+            }
+
+            public void Enqueue(T item)
+            {
+                if (!_isNowPush)
+                    ChangeActiveStack();
+                stack.Push(item);
+            }
+
+            public T Dequeue()
+            {
+                if (_isNowPush)
+                    ChangeActiveStack();
+                return stack.Pop();
+            }
+            private void ChangeActiveStack()
+            {
+                if (_isNowPush)
+                {
+                    while (stack.Size() > 0)
+                    {
+                        _popStack.Push(_pushStack.Pop());
+                    }
+                    _isNowPush = false;
+                }
+                else
+                {
+                    while (stack.Size() > 0)
+                    {
+                        _pushStack.Push(_popStack.Pop());
+                    }
+                    _isNowPush = true;
+                }
+            }
+
+
+            public int Size()
+            {
+                if (_isNowPush)
+                    return _pushStack.Size();
+                return _popStack.Size();
+            }
+        }
+    }
+}
