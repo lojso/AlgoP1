9a76c445c15563f928a99478408f9c7b9abd9fd5
Nikita Grishaev <grishaev@rightway.games>
2020-10-25 18:05:58 +0300

    Добавил поворот в обе стороны

--- AlgoP1/Task5Ad2.cs
+++ AlgoP1/Task5Ad2.cs
@@ -4,8 +4,16 @@ namespace AlgoP1
 {
     public static class Task5Ad2<T>
     {
-        public static Queue<T> RotateQueue(Queue<T> q, int n)
+        public static Queue<T> RotateQueueRight(Queue<T> q, int n)
         {
+            n = n % q.Size();
+            n = q.Size() - n;
+            return RotateQueueLeft(q, n);
+        }
+        
+        public static Queue<T> RotateQueueLeft(Queue<T> q, int n)
+        {
+            n = n % q.Size();
             for (int i = 0; i < n; i++)
             {
                 var val = q.Dequeue();
