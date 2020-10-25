b764cb951e6e54eea55d5a6eae6a027c2f5d8a9a
Nikita Grishaev <grishaev@rightway.games>
2020-10-25 17:33:10 +0300

    Переделал дополнительную задачу к пятому заданию

---
+++ AlgoP1/Task5Ad2.cs
@@ -0,0 +1,18 @@
+﻿using Task5;
+
+namespace AlgoP1
+{
+    public static class Task5Ad2<T>
+    {
+        public static Queue<T> RotateQueue(Queue<T> q, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                var val = q.Dequeue();
+                q.Enqueue(val);
+            }
+
+            return q;
+        }
+    }
+}
