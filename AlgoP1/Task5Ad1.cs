1b3725fae3b071cc9c59b17128d2e9ccbc05ccec
Nikita Grishaev <grishaev@rightway.games>
2020-10-25 14:23:31 +0300

    Обновил 5 дополнительную задачу

--- AlgoP1/Task5Ad1.cs
+++ AlgoP1/Task5Ad1.cs
@@ -6,11 +6,6 @@ namespace AlgoP1
     {
         public class Queue<T>
         {
-            private Stack<T> stack
-            {
-                get { return _isNowPush ? _pushStack : _popStack; }
-            }
-
             private Stack<T> _pushStack;
             private Stack<T> _popStack;
 
@@ -24,37 +19,23 @@ namespace AlgoP1
 
             public void Enqueue(T item)
             {
-                if (!_isNowPush)
-                    ChangeActiveStack();
-                stack.Push(item);
+                _pushStack.Push(item);
             }
 
             public T Dequeue()
             {
-                if (_isNowPush)
-                    ChangeActiveStack();
-                return stack.Pop();
-            }
-            private void ChangeActiveStack()
-            {
-                if (_isNowPush)
-                {
-                    while (stack.Size() > 0)
-                    {
-                        _popStack.Push(_pushStack.Pop());
-                    }
-                    _isNowPush = false;
-                }
-                else
-                {
-                    while (stack.Size() > 0)
-                    {
-                        _pushStack.Push(_popStack.Pop());
-                    }
-                    _isNowPush = true;
-                }
+                if (_popStack.Size() == 0)
+                    FillPopStack();
+                return _popStack.Pop();
             }
 
+            private void FillPopStack()
+            {
+                while (_pushStack.Size() != 0)
+                {
+                    _popStack.Push(_pushStack.Pop());
+                }
+            }
 
             public int Size()
             {
