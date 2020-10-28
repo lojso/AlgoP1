c74863a0851bd450cff6313d04a7a0ccea949195
Nikita Grishaev <grishaev@rightway.games>
2020-10-29 00:04:44 +0300

    Задача 7 + тесты

--- AlgoP1/Task7.cs
+++ AlgoP1/Task7.cs
@@ -4,29 +4,52 @@ using System.Collections.Generic;
 // https://skillsmart.ru/algo/15-121-cm/d5a98a594d.html
 namespace AlgorithmsDataStructures
 {
-
     public class Node<T>
     {
         public T value;
         public Node<T> next, prev;
+        public bool IsDummy { get; private set; }
 
         public Node(T _value)
         {
             value = _value;
+            IsDummy = false;
             next = null;
             prev = null;
+        }
+
+        public Node(T _value, bool isDummy) : this(_value)
+        {
+            IsDummy = isDummy;
         }
     }
 
     public class OrderedList<T>
     {
-        public Node<T> head, tail;
+        public Node<T> head
+        {
+            get { return _dummyHead.next; }
+        }
+
+        public Node<T> tail
+        {
+            get { return _dummyTail.prev; }
+        }
+
+        private Node<T> _dummyHead;
+        private Node<T> _dummyTail;
+        
         private bool _ascending;
 
         public OrderedList(bool asc)
         {
-            head = null;
-            tail = null;
+            Node<T> dummyFirst = new Node<T>(default(T), true);
+            Node<T> dummyLast = new Node<T>(default(T), true);
+            
+            _dummyHead = dummyFirst;
+            _dummyTail = dummyLast;
+
+            Clear(asc);
             _ascending = asc;
         }
 
@@ -35,44 +58,84 @@ namespace AlgorithmsDataStructures
             int result = 0;
             if(typeof(T) == typeof(String))
             {
-                // версия для лексикографического сравнения строк
+                result = string.Compare(v1 as string, v2 as string);
             }
-            else 
+            else
             {
-                // универсальное сравнение
+                result = Comparer<T>.Default.Compare(v1, v2);
             }
       
             return result;
-            // -1 если v1 < v2
-            // 0 если v1 == v2
-            // +1 если v1 > v2
+        }
+
+        private bool IsBigger(T v1, T v2)
+        {
+            return Compare(v1, v2) > 0;
+        }
+        
+        private bool IsLower(T v1, T v2)
+        {
+            return Compare(v1, v2) < 0;
         }
 
         public void Add(T value)
         {
-            // автоматическая вставка value 
-            // в нужную позицию
+            if (Count() == 0)
+            {
+                InsertBefore(head, new Node<T>(value));
+                return;
+            }
+            
+            var curNode = head;
+            while (!curNode.IsDummy)
+            {
+                if (IsBigger(curNode.value, value) && _ascending ||
+                    IsLower(curNode.value, value) && !_ascending)
+                {
+                    InsertBefore(curNode, new Node<T>(value));
+                    return;
+                }
+                curNode = curNode.next;
+            }
+            InsertAfter(tail, new Node<T>(value));
         }
 
         public Node<T> Find(T val)
         {
+            var curNode = head;
+            while (!curNode.IsDummy)
+            {
+                if (curNode.value.Equals(val))
+                    return curNode;
+                curNode = curNode.next;
+            }
+            
             return null; // здесь будет ваш код
         }
 
         public void Delete(T val)
         {
-            // здесь будет ваш код
+            RemoveNode(Find(val));
         }
 
         public void Clear(bool asc)
         {
             _ascending = asc;
-            // здесь будет ваш код
+            _dummyHead.next = _dummyTail;
+            _dummyTail.prev = _dummyHead;
         }
 
         public int Count()
         {
-            return 0; // здесь будет ваш код подсчёта количества элементов в списке
+            int count = 0;
+            var curNode = head;
+            while (!curNode.IsDummy)
+            {
+                count += 1;
+                curNode = curNode.next;
+            }
+
+            return count;
         }
 
         List<Node<T>> GetAll() // выдать все элементы упорядоченного 
@@ -86,6 +149,60 @@ namespace AlgorithmsDataStructures
                 node = node.next;
             }
             return r;
+        }
+        
+        public override string ToString()
+        {
+            string result = "";
+            var curNode = head;
+            while (!curNode.IsDummy)
+            {
+                result += curNode.value + " ";
+                curNode = curNode.next;
+            }
+
+            return result.TrimEnd();
+        }
+        
+        private void InsertAfter(Node<T> _nodeAfter, Node<T> _nodeToInsert)
+        {
+            if (_nodeAfter == null)
+            {
+                InsertBefore(head, _nodeToInsert);
+            }
+            else
+            {
+                _nodeToInsert.prev = _nodeAfter;
+                _nodeToInsert.next = _nodeAfter.next;
+
+                _nodeToInsert.prev.next = _nodeToInsert;
+                _nodeToInsert.next.prev = _nodeToInsert;
+            }
+        }
+
+        private void InsertBefore(Node<T> _nodeBefore, Node<T> _nodeToInsert)
+        {
+            if (_nodeBefore == null)
+            {
+                InsertAfter(tail, _nodeToInsert);
+            }
+            else
+            {
+                _nodeToInsert.next = _nodeBefore;
+                _nodeToInsert.prev = _nodeBefore.prev;
+
+                _nodeToInsert.prev.next = _nodeToInsert;
+                _nodeToInsert.next.prev = _nodeToInsert;
+            }
+        }
+        
+        private void RemoveNode(Node<T> node)
+        {
+            if(node == null)
+                return;
+            
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
         }
     }
  
---
+++ Tests/Task7.cs
@@ -0,0 +1,128 @@
+﻿using AlgorithmsDataStructures;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Task7
+    {
+        
+        [Test]
+        public void TestAddingItemsAsc()
+        {
+            var stack = new OrderedList<int>(true);
+            
+            stack.Add(1);
+            stack.Add(3);
+            stack.Add(5);
+            stack.Add(0);
+            stack.Add(2);
+            stack.Add(7);
+            stack.Add(4);
+            stack.Add(4);
+            
+            Assert.True(stack.ToString().Equals("0 1 2 3 4 4 5 7"));
+        }
+        
+        [Test]
+        public void TestAddingItemsDec()
+        {
+            var stack = new OrderedList<int>(false);
+            
+            stack.Add(1);
+            stack.Add(3);
+            stack.Add(5);
+            stack.Add(0);
+            stack.Add(2);
+            stack.Add(7);
+            stack.Add(4);
+            stack.Add(4);
+            
+            Assert.True(stack.ToString().Equals("7 5 4 4 3 2 1 0"));
+        }
+        
+        [Test]
+        public void TestRemoveAsc()
+        {
+            var stack = new OrderedList<int>(true);
+            stack.Add(1);
+            stack.Add(3);
+            stack.Add(5);
+            stack.Add(0);
+            stack.Add(2);
+            stack.Add(7);
+            stack.Add(4);
+            stack.Add(4);
+            
+            stack.Delete(0);
+            stack.Delete(2);
+            stack.Delete(4);
+            stack.Delete(7);
+            
+            Assert.True(stack.ToString().Equals("1 3 4 5"));
+        }
+        
+        [Test]
+        public void TestRemoveDes()
+        {
+            var stack = new OrderedList<int>(false);
+            stack.Add(1);
+            stack.Add(3);
+            stack.Add(5);
+            stack.Add(0);
+            stack.Add(2);
+            stack.Add(7);
+            stack.Add(4);
+            stack.Add(4);
+            
+            stack.Delete(0);
+            stack.Delete(2);
+            stack.Delete(4);
+            stack.Delete(7);
+            
+            Assert.True(stack.ToString().Equals("5 4 3 1"));
+        }
+        
+        [Test]
+        public void TestClearDec()
+        {
+            var stack = new OrderedList<int>(false);
+            stack.Add(1);
+            stack.Add(3);
+            stack.Add(5);
+            stack.Add(0);
+            stack.Add(2);
+            stack.Add(7);
+            stack.Add(4);
+            stack.Add(4);
+            
+            stack.Clear(false);
+            stack.Add(1);
+            stack.Add(2);
+            
+            Assert.True(stack.ToString().Equals("2 1"));
+            Assert.True(stack.Count() == 2);
+        }
+        
+        [Test]
+        public void TestClearAsc()
+        {
+            var stack = new OrderedList<int>(true);
+            stack.Add(1);
+            stack.Add(3);
+            stack.Add(5);
+            stack.Add(0);
+            stack.Add(2);
+            stack.Add(7);
+            stack.Add(4);
+            stack.Add(4);
+            
+            stack.Clear(true);
+            stack.Add(1);
+            stack.Add(2);
+            
+            Assert.True(stack.ToString().Equals("1 2"));
+            Assert.True(stack.Count() == 2);
+        }
+    }
+}
